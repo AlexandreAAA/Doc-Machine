e0283129245bb5a3bc329cb37c8bb89fcdac563f
EDK-pro <118174383+EDK-pro@users.noreply.github.com>
2024-12-18 11:13:48 +0100

    G réparé

--- Script/main.gd
+++ Script/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var robot : Node2D
 @export var end_timer_time := 120.0
 @export var end_timer : Timer
-var bot = preload("res://Scene/robot.tscn")
+
 
 func _ready() -> void:
 	end_timer.wait_time = end_timer_time
@@ -12,8 +12,3 @@ func _process(delta: float) -> void:
 	
 	if end_timer.time_left <= 110:
 		pass
-
-#func _spawn_robot():
-	#var robot = bot.instantiate()
-	#add_child(robot)
-	#pass
