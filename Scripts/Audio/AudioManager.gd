e25f7c3ae025272492bd2d2495fc8d7e080ae990
AlexandreAAA <76970095+AlexandreAAA@users.noreply.github.com>
2024-12-18 13:52:54 +0100

    Sound catégorisé pre record

--- Scripts/Audio/AudioManager.gd
+++ Scripts/Audio/AudioManager.gd
@@ -17,38 +17,40 @@ var VoiceBank = {}
 
 var OST = preload("res://Audio/Music/OST.ogg")
 
-var ca_va_mieux = preload("res://Audio/Voice/Bien/Ca va mieux.ogg")
-var peche = preload("res://Audio/Voice/Bien/J'ai la peche.ogg")
-var oh_oui = preload("res://Audio/Voice/Bien/Oh Oui.ogg")
-var wesh = preload("res://Audio/Voice/Bien/Wesh ma gueule.ogg")
 
-var GoodDoigt = preload("res://Audio/Voice/Bien/GoodDoigt.ogg")
-var GoodExp = preload("res://Audio/Voice/Bien/GoodExp.ogg")
+#Good
+var GoodFil = preload("res://Audio/Voice/Bien/GoodDoigt.ogg")
+var GoodJack = preload("res://Audio/Voice/Bien/GoodExp.ogg")
+var GoodMieux = preload("res://Audio/Voice/Bien/Ca va mieux.ogg")
+var Goodpeche = preload("res://Audio/Voice/Bien/J'ai la peche.ogg")
+var GoodOui = preload("res://Audio/Voice/Bien/Oh Oui.ogg")
+var GoodWesh = preload("res://Audio/Voice/Bien/Wesh ma gueule.ogg")
 
-var adieu = preload("res://Audio/Voice/Pas bien/Adieu monde cruel.ogg")
-var aide_moi = preload("res://Audio/Voice/Pas bien/Aide-moi.ogg")
-var je_pars = preload("res://Audio/Voice/Pas bien/Ca y est je pars.ogg")
-var gingle_netflix = preload("res://Audio/Voice/Pas bien/Gingle Netflix.ogg")
-var mal = preload("res://Audio/Voice/Pas bien/J'ai Mal.ogg")
-var pire = preload("res://Audio/Voice/Pas bien/Pire.ogg")
-var BadContact = preload("res://Audio/Voice/Pas bien/BadContact.ogg")
-var BadDef = preload("res://Audio/Voice/Pas bien/BadDef.ogg")
-var BadEspa = preload("res://Audio/Voice/Pas bien/BadEspa.ogg")
-var BadPort = preload("res://Audio/Voice/Pas bien/BadPort.ogg")
-var BadTerm = preload("res://Audio/Voice/Pas bien/BadTerm (1).ogg")
-var BadVirus = preload("res://Audio/Voice/Pas bien/BadVirus.ogg")
+#Bad
+var BadFil = preload("res://Audio/Voice/Pas bien/BadContact.ogg")
+var BadBouton = preload("res://Audio/Voice/Pas bien/BadDef.ogg")
+var BadEnceinte = preload("res://Audio/Voice/Pas bien/BadEspa.ogg")
+var BadCable_alu = preload("res://Audio/Voice/Pas bien/BadPort.ogg")
+var BadGpu = preload("res://Audio/Voice/Pas bien/BadTerm (1).ogg")
+var BadRam = preload("res://Audio/Voice/Pas bien/BadVirus.ogg")
 
-var depeche = preload("res://Audio/Voice/Pas bien/Depeche.ogg")
-var pas_partir = preload("res://Audio/Voice/Pas bien/Je ne veux pas partir.ogg")
-var fais_qqchose = preload("res://Audio/Voice/Pas bien/Fais quelque chose.ogg")
-var sens_le_cloud = preload("res://Audio/Voice/Pas bien/Je sens le cloud.ogg")
-var laisse_pas = preload("res://Audio/Voice/Pas bien/Ne me laisse pas.ogg")
-
+#Idle
 var IdleDEFRAG = preload("res://Audio/Voice/Idle/IdleFRAG.ogg")
-var IdleE = preload("res://Audio/Voice/Idle/IdleIE.ogg")
+var IdleIE = preload("res://Audio/Voice/Idle/IdleIE.ogg")
 var IdleUpdate = preload("res://Audio/Voice/Idle/IdleUpdate.ogg")
 var IdleVlc = preload("res://Audio/Voice/Idle/IdleVLC (1).ogg")
-
+#//Synthese Idle
+var IdleDepeche = preload("res://Audio/Voice/Pas bien/Depeche.ogg")
+var IdlePas_partir = preload("res://Audio/Voice/Pas bien/Je ne veux pas partir.ogg")
+var IdleSens_le_cloud = preload("res://Audio/Voice/Pas bien/Je sens le cloud.ogg")
+var IdleLaisse_pas = preload("res://Audio/Voice/Pas bien/Ne me laisse pas.ogg")
+var IdleAdieu = preload("res://Audio/Voice/Pas bien/Adieu monde cruel.ogg")
+var IdleAide_Moi = preload("res://Audio/Voice/Pas bien/Aide-moi.ogg")
+var IdleJe_Pars = preload("res://Audio/Voice/Pas bien/Ca y est je pars.ogg")
+var IdleFais_qqchose = preload("res://Audio/Voice/Pas bien/Fais quelque chose.ogg")
+var IdleGingle_Netflix = preload("res://Audio/Voice/Pas bien/Gingle Netflix.ogg")
+var IdleMal = preload("res://Audio/Voice/Pas bien/J'ai Mal.ogg")
+var IdlePire = preload("res://Audio/Voice/Pas bien/Pire.ogg")
 
 func _playAtInput() -> void:
 	if Input.is_action_pressed("TestSon") :
@@ -59,6 +61,8 @@ func _changeClipAtInput(plusOne : int) :
 		print("Son Changer")
 		
 		
+		
+#Methodes 
 func _alarmPlay() -> void :
 	AlarmStream.play()
 	
@@ -89,38 +93,37 @@ func _musicStop() -> void :
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	VoiceSyntheseBank["GoodMieux"] = ca_va_mieux
-	VoiceSyntheseBank["Goodpeche"] = peche
-	VoiceSyntheseBank["GoodOui"] = oh_oui
-	VoiceSyntheseBank["GoodWesh"] = wesh
-	VoiceSyntheseBank["BadAdieu"] = adieu
-	VoiceSyntheseBank["BadAide"] = aide_moi
-	VoiceSyntheseBank["BadPars"] = je_pars
-	VoiceSyntheseBank["BadPire"] = pire 
-	VoiceSyntheseBank["BadGingle"] = gingle_netflix
-	VoiceSyntheseBank["BadMal"] = mal
-	VoiceSyntheseBank["IdleDepeche"] = depeche
-	VoiceSyntheseBank["IdleFaisquelquechose"] = fais_qqchose
-	VoiceSyntheseBank["IdlePasPartir"] = pas_partir
-	VoiceSyntheseBank["IdleCloud"] = sens_le_cloud
-	VoiceSyntheseBank["IdleLaissePas"] = laisse_pas
-	
+	VoiceSyntheseBank["GoodMieux"] = GoodMieux
+	VoiceSyntheseBank["Goodpeche"] = Goodpeche
+	VoiceSyntheseBank["GoodOui"] = GoodOui
+	VoiceSyntheseBank["GoodWesh"] = GoodWesh
+	VoiceSyntheseBank["IdleAdieu"] = IdleAdieu
+	VoiceSyntheseBank["IdleAide_Moi"] = IdleAide_Moi
+	VoiceSyntheseBank["IdleJe_Pars"] = IdleJe_Pars
+	VoiceSyntheseBank["IdlePire"] = IdlePire 
+	VoiceSyntheseBank["IdleGingle_Netflix"] = IdleGingle_Netflix
+	VoiceSyntheseBank["IdleMal"] = IdleMal
+	VoiceSyntheseBank["IdleDepeche"] = IdleDepeche
+	VoiceSyntheseBank["IdleFais_qqchose"] = IdleFais_qqchose
+	VoiceSyntheseBank["IdlePas_partir"] = IdlePas_partir
+	VoiceSyntheseBank["IdleSens_le_cloud"] = IdleSens_le_cloud
+	VoiceSyntheseBank["IdleLaisse_pas"] = IdleLaisse_pas
 	
 	
 	VoiceBank["IdleDEFRAG"] = IdleDEFRAG
-	VoiceBank["IdleE"] = IdleE
+	VoiceBank["IdleIE"] = IdleIE
 	VoiceBank["IdleUpdate"] = IdleUpdate
 	VoiceBank["IdleVlc"] = IdleVlc
 	
-	VoiceBank["BadContact"] = BadContact
-	VoiceBank["BadDef"] = BadDef
-	VoiceBank["BadEspa"] = BadEspa
-	VoiceBank["BadPort"] = BadPort
-	VoiceBank["BadTerm"] = BadTerm
-	VoiceBank["BadVirus"] = BadVirus
+	VoiceBank["BadFil"] = BadFil
+	VoiceBank["BadBouton"] = BadBouton
+	VoiceBank["BadEnceinte"] = BadEnceinte
+	VoiceBank["BadCable_alu"] = BadCable_alu
+	VoiceBank["BadGpu"] = BadGpu
+	VoiceBank["BadRam"] = BadRam
 	
-	VoiceBank["GoodDoigt"] = GoodDoigt
-	VoiceBank["GoodExp"] = GoodExp
+	VoiceBank["GoodFil"] = GoodFil
+	VoiceBank["GoodJack"] = GoodJack
 	
 
 	
@@ -134,6 +137,10 @@ func _process(delta: float) -> void:
 
 func _playVoiceLine(voiceBankKey : String) -> void :
 	VoiceStream.stream = VoiceBank[voiceBankKey]
+	VoiceStream.play()
+	
+func _playSynthVoiceLine(synthVoiceBankKey : String) -> void : 
+	VoiceStream.stream = VoiceSyntheseBank[synthVoiceBankKey]
 	VoiceStream.play()
 	
 	
