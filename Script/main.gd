46b4cb2d132ead40d85ecc726ac3a9736811f764
Poire <49816485+PoireaDoxe@users.noreply.github.com>
2024-12-18 15:23:48 +0100

    push download window
    la dl window apparaît du bas de l'écran

--- Script/main.gd
+++ Script/main.gd
@@ -5,18 +5,25 @@ extends Node2D
 @export var end_timer : Timer
 @export var download_window : Sprite2D
 @export var prog_bar : TextureProgressBar
+@export var dl_time_display : RichTextLabel
 @export var dl_started : bool
-var pb_value : float
+@export var destination : Vector2
+@export var speed : float
 
 func _ready() -> void:
 	end_timer.wait_time = end_timer_time
 	download_window.visible = false
 	end_timer.start(end_timer_time)
-
+	download_window.transform.origin = Vector2(600,600)
+	download_window.scale = Vector2(0.1, 0.1)
+	destination = Vector2(600,80)
+	speed = 1500.0
+	
 func _process(delta: float) -> void:
 	if !dl_started :
 		dl_start()
 	prog_bar.value = (end_timer_time - end_timer.time_left)
+	update_displayed_time()
 	if end_timer.time_left <= 110:
 		pass
 
@@ -26,5 +33,9 @@ func _on_end_timer_timeout() -> void:
 func dl_start():
 	dl_started = true
 	download_window.visible = true	
-	prog_bar.max_value = end_timer_time
-	pass
+	var tween = get_tree().create_tween()
+	tween.parallel().tween_property(download_window,"position", destination, 0.3)
+	tween.parallel().tween_property(download_window,"scale", Vector2(0.6, 0.6), 0.3)
+	
+func update_displayed_time():
+	dl_time_display.text = str(snapped(end_timer.time_left/60, 0.01)) + " minutes restantes"
